3689adc85d3d49964324b27f248cc0941bba0ea8
Sam <doansamquoc@gmail.com>
2025-08-30 12:29:23 +0700

    First commit

---
+++ src/main/java/com/sam/chattogether/ChatTogetherApplication.java
@@ -0,0 +1,13 @@
+package com.sam.chattogether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatTogetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatTogetherApplication.class, args);
+	}
+
+}
